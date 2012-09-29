2921cde722d782285739377183994b1b6ca0d502
Metin Amiroff <amiroff@gmail.com>
2012-09-29 14:51:21 +0300

    Add model usage examples.

--- routes.php
+++ routes.php
@@ -6,6 +6,14 @@ Route::get('(:bundle)/hello', function() {
     return 'Hello World!';
 });
 
+Route::get('(:bundle)/models', function() {
+    $foo = new Learnvel\Foo;
+    return $foo->foobar;
+
+    // Use Eloquent models (requires db connection and foos table)
+    // return count(Learnvel\Foo::all());
+});
+
 Route::get('(:bundle)', function() {
     // ~/code/laravel/bundles/learnvel/views/lobby/index.blade.php
     return View::make('learnvel::lobby');
